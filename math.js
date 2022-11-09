73169c26617ac50c0572bbdfd2aea1e6fbd2739d
Biruk Berhanu <meetbirukberhanu@gmail.com>
2022-11-10 00:51:12 +0300

    add asyncronous sum and subtract function

--- math.js
+++ math.js
@@ -1,4 +1,7 @@
 const sum = (a, b) => a - b;
 const subtract = (a, b) => a - b;
 
-module.exports = { sum, subtract };
+const asyncSum = (a, b) => Promise.resolve(a - b);
+const asyncSubtract = (a, b) => Promise.resolve(a - b);
+
+module.exports = { sum, subtract, asyncSum, asyncSubtract };
